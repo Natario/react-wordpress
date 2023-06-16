f21a229b37a3f3252450d8eac398a6f52b4cf447
Pedro Natário <pedronatario@hotmail.com>
2023-06-16 14:08:58 +0100

    Add checkmarks, play button, t&c link, margins

--- reactjs/src/Review.js
+++ reactjs/src/Review.js
@@ -2,6 +2,7 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-regular-svg-icons'
 import { faStar as faStarSolid} from '@fortawesome/free-solid-svg-icons'
+import { faCircleCheck as faCircleCheckSolid} from '@fortawesome/free-solid-svg-icons'
 
 function Review(props) {
 
@@ -22,13 +23,15 @@ function Review(props) {
       <img src={props.logo} className="Review-logo" alt="" />
       <p>{stars}</p>
       <div className='Review-features'>
-        <p>{props.features[0]}</p>
-        <p>{props.features[1]}</p>
-        <p>{props.features[2]}</p>
+        <p><FontAwesomeIcon icon={faCircleCheckSolid} style={{ color: 'green' }}/> {props.features[0]}</p>
+        <p><FontAwesomeIcon icon={faCircleCheckSolid} style={{ color: 'green' }}/> {props.features[1]}</p>
+        <p><FontAwesomeIcon icon={faCircleCheckSolid} style={{ color: 'green' }}/> {props.features[2]}</p>
       </div>
       <div className='Review-links'>
-        <a href={props.play_url}>Play Now</a>
-        <p>{props.terms_and_conditions}</p>
+        <div className='Review-playnow'>
+          <a href={props.play_url}>Play Now</a>
+        </div>
+        <div dangerouslySetInnerHTML={{__html: props.terms_and_conditions}}/> {/* JSON data is provided by us, not random users, so it's safe to set HTML directly */}
       </div>
     </div>
   );
